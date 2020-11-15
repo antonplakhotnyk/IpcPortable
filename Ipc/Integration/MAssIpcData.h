e1042c234ee00e1d25f4fd5cca9a9663b8b609bb
Anton <antonix@xitona.com>
2020-11-15 20:14:51 +0200

    missing file added

---
+++ Ipc/Integration/MAssIpcData.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+class MAssIpcData
+{
+public:
+
+	typedef uint32_t TPacketSize;
+
+	virtual ~MAssIpcData() = default;
+
+	virtual TPacketSize Size() const = 0;
+	virtual uint8_t* Data() = 0;
+};
